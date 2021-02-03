92d8ce8e46b3bb24975651419dfd83c9ab69df61
Aniket sangohi <manjaniket@gmail.com>
2021-02-03 15:31:02 +0530

    adding code...

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');//create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response
+  res.write('Hello World! brijesh '); //write a response
   res.end(); //end the response
 }).listen(3000, function(){
  console.log("server start at port 3000"); //the server object listens on port 3000
